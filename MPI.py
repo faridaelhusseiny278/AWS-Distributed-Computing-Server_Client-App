83e8e4c09cefde75539e57d0ab4e67e9b4f26928
Farida Elhusseiny <110945939+faridaelhusseiny278@users.noreply.github.com>
2024-05-09 21:26:12 +0300

    Add files via upload

--- MPI.py
+++ MPI.py
@@ -11,7 +11,6 @@ from Image_Processing import edge_detection, color_manipulation
 from PIL import Image
 import cv2
 
-
 class WorkerThread(threading.Thread):
     def __init__(self, comm, task_queue):
         threading.Thread.__init__(self)
@@ -20,7 +19,7 @@ class WorkerThread(threading.Thread):
 
     def run(self):
                 image, operation = self.task_queue
-                print(f"Worker {self.comm.Get_rank()} is now processing task.{self.task_queue}")
+                print(f"Worker {self.comm.Get_rank()} is now processing task.")
                 
                 result = self.process_image(image, operation)
                 print(f"result is now preprocessed by worker {self.comm.Get_rank()}")
@@ -50,65 +49,44 @@ class WorkerThread(threading.Thread):
 
 task_queue = queue.Queue()
 def handle_client(client_socket):
-    operation = None
-    new_image = False
+    
+    tasks = []
+    recv_data = client_socket.recv(BUFFER_SIZE)
     file_stream = io.BytesIO()
-    while True:
+    file_stream.write(recv_data)
 
-        if not new_image: 
+    while b'###%Image_End%' not in recv_data:
             recv_data = client_socket.recv(BUFFER_SIZE)
-            while recv_data:
-                    
-                    if recv_data ==b"new_image":
-                        client_socket.send(b"new_image_ok")
-                        print("New image request!")
-                        break
-                    recv_data = client_socket.recv(BUFFER_SIZE)
-
-        recv_data = client_socket.recv(BUFFER_SIZE)
-        while recv_data:
-                print("Doing (",recv_data,") operation...")
-                if recv_data == b"Edge_detection":
-                    operation = 1
-                    client_socket.send(b"Operation_ok")
-                    break
-                elif recv_data == b"color_manipulation":
-                    operation = 2
-                    client_socket.send(b"Operation_ok")
-                    break
-                recv_data = client_socket.recv(BUFFER_SIZE)
-
-        recv_data = client_socket.recv(BUFFER_SIZE)
-        while recv_data:
             file_stream.write(recv_data)
-            recv_data = client_socket.recv(BUFFER_SIZE)
-
             if b"###%Image_Sent%" in recv_data:
                         end_image = recv_data.split(b"###")
                         if len(end_image[0])>0:
                             file_stream.write(end_image[0])
-                            
-                        break
-                            
-        
-        file_stream.seek(0)  
-        image_data = np.frombuffer(file_stream.getvalue(), dtype=np.uint8)
-        image = cv2.imdecode(image_data, cv2.IMREAD_COLOR)
-        
+
+                        client_socket.send(b"I got the file")
+                        operation = client_socket.recv(BUFFER_SIZE)
+
+                        file_stream.seek(0)  
+                        image_data = np.frombuffer(file_stream.getvalue(), dtype=np.uint8)
+                        image = cv2.imdecode(image_data, cv2.IMREAD_COLOR)
+                        tasks.append((image, int(operation)))
+                        file_stream = io.BytesIO()
+
        
-        tasks= [(image, operation),(None)]
-        for task in tasks:
-            task_queue.put(task)
+    tasks.append(None) 
+    for task in tasks:
+        task_queue.put(task)
 
-    
-        return task_queue
+    return task_queue
+
 
         
-
+print ("My rank is ", MPI.COMM_WORLD.Get_rank(), "My hostname is ", MPI.Get_processor_name())
 if MPI.COMM_WORLD.Get_rank() == 0:
+    processed_images = []   
 
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # = ip
-    server.bind(('0.0.0.0',50321))
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
+    server.bind(('0.0.0.0',55552))
     server.listen()
     BUFFER_SIZE = 4096
     print("Server is listening for connections.....")
@@ -126,41 +104,45 @@ if MPI.COMM_WORLD.Get_rank() == 0:
                 task = task_queue.get()
                 if task is not None:
                     MPI.COMM_WORLD.send(task, dest=i)
+                    print(f"sent work to worker {i}")
                 else:
                     break
             
-           
-
-        print("Master waiting for result from worker...")
-        for i in range(1, num_workers):
+        print("task queue size: ", task_queue.qsize())  
+        for i in range(1,MPI.COMM_WORLD.Get_size()):
+            print(f"Master waiting for result from worker{i}")
             image = MPI.COMM_WORLD.recv(source=i)
-            print("Master received result from worker...", image)
+            processed_images.append(image)
+        
+            image.save(f'server_file_{i}.jpeg', format='JPEG')
+            
+            image = Image.open(f'server_file_{i}.jpeg')
+            image_array = np.array(image)
 
-            image.save('server_file.jpeg', format='JPEG')
 
-            with open('server_file.jpeg','rb') as file:
-                file_data =file.read(BUFFER_SIZE)
 
+        for i in range(1,MPI.COMM_WORLD.Get_size()): 
+            with open(f'server_file_{i}.jpeg', 'rb') as file:
+                file_data = file.read(BUFFER_SIZE)
                 while file_data:
                     client_socket.send(file_data)
                     file_data = file.read(BUFFER_SIZE)
+                client_socket.send(b"###%Image_Sent%")
+                if client_socket.recv(BUFFER_SIZE) == b"I got the file":
+                    continue
+                      
+            # os.unlink(f'server_file_{i}.jpeg')
+            print(f"Processed Image {i} Sent back")
 
-            client_socket.send(b"###%Image_Sent%")
-            
-            os.unlink('server_file.jpeg')
-            file.close()
-            print("Processed Image Sent back ^^")
 
-        recv_data = client_socket.recv(BUFFER_SIZE)
-        if b"Close_Connection" in recv_data:
-                print(f"Client {client_socket.getpeername()} Closed Connection!")
-                client_socket.close()
-                break
-        
+        client_socket.close() 
+        break  
+          
 
 else:
                 task_queue = MPI.COMM_WORLD.recv(source=0)
-                print("Worker", MPI.COMM_WORLD.Get_rank(), "received task", task_queue)
+                print("Worker", MPI.COMM_WORLD.Get_rank(), "received task")
+         
                 WorkerThread(MPI.COMM_WORLD, task_queue).start()
 
 
